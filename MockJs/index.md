a6588ee708fe19e18112de0e967a6cb4bc720da4
bigkaiwu <bigkaiwu@163.com>
2020-01-06 16:16:17 +0800

    add mockjs

---
+++ MockJs/index.md
@@ -0,0 +1,7 @@
+`npm install axios` 安装axios
+
+`npm install mockjs`安装mockjs
+
+### 全局配置
+
+[官方文档](https://github.com/nuysoft/Mock/wiki)
---
+++ MockJs/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import request from '@/http/request'
+export default {
+  mounted () {
+    this.getData()
+  },
+  methods: {
+      getData() {
+          request.http_mock('http://test.mockjs.com/test').then(response => {
+              window.console.log(response._data)
+          })
+      }  
+  }
+}
+</script>
